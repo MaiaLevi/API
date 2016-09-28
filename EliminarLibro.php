bfd993591e7a313166e7cad2ceb621116b3b18d7
DAIANA KALANIT SZWIMER <42038123@ort>
2016-09-28 13:54:25 -0300

    anda

--- EliminarLibro.php
+++ EliminarLibro.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect('ca-cdbr-azure-central-a.cloudapp.net','b926aabc612c57','a843969f','proyecto');
+$con=mysqli_connect('ca-cdbr-azure-central-a.cloudapp.net','b926aabc612c57','a843969f','acsm_55ece9aded5c035');
 if (mysqli_connect_errno()) {
    echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
