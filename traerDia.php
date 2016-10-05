c2e9fe36e95b034c5c2c5539f96601c0ac44cf88
DAIANA KALANIT SZWIMER <42038123@ort>
2016-10-05 13:49:06 -0300

    trae idmateria

--- traerDia.php
+++ traerDia.php
@@ -7,7 +7,7 @@ if (mysqli_connect_errno()) {
 //$res=$stmt->get_result();
 $id=$_GET["IdDivision"];
 $dia=$_GET["Dia"];
-$query="SELECT horario.bloque, materia.Nombre AS Materia FROM horario INNER JOIN materia ON horario.idmateria=materia.IdMateria WHERE horario.iddivision='$id' AND horario.idsemana='$dia' ORDER BY horario.bloque ASC";
+$query="SELECT horario.bloque, materia.Nombre AS Materia, horario.idmateria AS IdMateria FROM horario INNER JOIN materia ON horario.idmateria=materia.IdMateria WHERE horario.iddivision='$id' AND horario.idsemana='$dia' ORDER BY horario.bloque ASC";
 //$stmt->bind_result($col1, $col2);
 $result = mysqli_query($con, $query);
 $objetos = array();
@@ -15,7 +15,8 @@ $objetos = array();
 		{ 
 			$Bloque=$row['bloque'];
 			$Materia=$row['Materia'];
-			$objeto = array('Bloque'=>$Bloque,'Materia'=> $Materia);	
+			$IdMateria=$row['IdMateria'];
+			$objeto = array('Bloque'=>$Bloque,'Materia'=> $Materia, 'IdMateria'=>$IdMateria);	
 			$objetos[] = $objeto;
 		}
 $close = mysqli_close($con) 
