5068ed430076df89ab9aa99948def99c79859763
DAIANA KALANIT SZWIMER <42038123@ort>
2016-11-04 08:58:05 -0300

    sajdka

--- notificaciones.php
+++ notificaciones.php
@@ -6,8 +6,8 @@ if (mysqli_connect_errno()) {
 //$res=$stmt->get_result();
 $id=$_GET["IdUsuario"];
 date_default_timezone_get();
-$from_date = date('Y-m-d',strtotime($Date. ' + 7 days'), time());
-//"SELECT * FROM logs WHERE date BETWEEN '" . $from_date . "' AND  '" . $to_date . "'ORDER by id DESC"
+$from_date = date('Y-m-d',strtotime($from_date. ' + 7 days'), time());
+echo $from_date;
 $query="SELECT evento.Id, evento.Fecha, evento.Descripcion,evento.IdMateria, materia.Nombre AS Materia, evento.IdTipo, tipo.Nombre AS Tipo FROM evento INNER JOIN materia ON evento.IdMateria=materia.IdMateria INNER JOIN tipo ON evento.IdTipo=tipo.IdTipo WHERE IdUsuario='$id' AND evento.Fecha BETWEEN '" . $from_date . "' AND  '" . $from_date . "' ORDER BY evento.Fecha ASC";
 //$stmt->bind_result($col1, $col2);
 $result = mysqli_query($con, $query);
@@ -23,7 +23,6 @@ $objetos = array();
 			$Tipo=$row['Tipo'];
 			$objeto = array('Id'=> $Id, 'Fecha'=> $Fecha, 'Descripcion'=> $Descripcion,'IdMateria'=>$IdMateria, 'Materia'=> $Materia, 'IdTipo'=>$IdTipo,'Tipo'=> $Tipo);	
 				$objetos[] = $objeto;
-			
 		}
 $close = mysqli_close($con) 
 or die("Ha sucedido un error inesperado en la desconexion de la base de datos");
