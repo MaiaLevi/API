847826a0400bc6cf324fd508b0ef78bd6f22e8d8
MaiaLevi <maianatalilevi@gmail.com>
2016-11-20 01:18:42 -0300

    Update agregarHorario.php

--- agregarHorario.php
+++ agregarHorario.php
@@ -8,6 +8,29 @@ if (mysqli_connect_errno()) {
 }		
 $string = file_get_contents('php://input');
 $horario=json_decode($string,true);
+$boolean=true;
+$query="SELECT iddivision, bloque, idsemana FROM horario WHERE iddivision=? AND bloque=? AND idsemana=?";
+$stmt=$con->prepare($query);
+$stmt->bind_param(
+		'iii',
+		$horario["iddivision"],
+		$horario["bloque"],
+		$horario["idsemana"]
+		);
+		$stmt->execute();
+//primero hacer select, si devuelve vacio se agrega sino no
+$objetos = array();
+while($row = mysqli_fetch_array($result)) 
+{
+	$objeto = array('Respuesta'=> "false");	
+	$objetos[] = $objeto;
+	$boolean=false;
+			
+}
+$json_string = json_encode(array('result' => $objetos), JSON_UNESCAPED_UNICODE );
+echo $json_string;
+if ($boolean)
+{
 $query = "INSERT INTO horario (iddivision, bloque, idsemana, idmateria) VALUES (?, ?, ?, ?)";
 $stmt=$con->prepare($query);
 $stmt->bind_param(
@@ -18,5 +41,7 @@ $stmt->bind_param(
 		$horario["idmateria"]
 		);
 		$stmt->execute();
+}
+
 mysqli_close($con);
 ?>
