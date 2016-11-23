1fb9b48e418ee086f577dc348578dcda2d8506dc
MaiaLevi <maianatalilevi@gmail.com>
2016-11-23 14:50:56 -0300

    Update modificarUsuario.php

--- modificarUsuario.php
+++ modificarUsuario.php
@@ -1,6 +1,5 @@
 <?php
 $con=mysqli_connect('ca-cdbr-azure-central-a.cloudapp.net','b926aabc612c57','a843969f','acsm_55ece9aded5c035');
-
 if (mysqli_connect_errno()) {
     printf("Error de conexión: %s\n", mysqli_connect_error());
     exit();
@@ -11,7 +10,7 @@ var_dump($string);
 var_dump($usuario);
 $stmt = $con->prepare("UPDATE usuario SET nombre=?, apellido=?, mail=?, celular=?, fechanacimiento=?, iddivision=?
 WHERE idusuario=?");
-$stmt->bind_param('sssisi',
+$stmt->bind_param('sssisii',
 		$usuario["nombre"],
 		$usuario["apellido"],
 		$usuario["mail"],
